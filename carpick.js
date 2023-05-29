89288da00983fb12f617ab9d6aa50f76a96c80d5
Evthedetailer <134971188+Evthedetailer@users.noreply.github.com>
2023-05-29 17:14:12 -0400

    Update carpick.js

--- carpick.js
+++ carpick.js
@@ -38,7 +38,7 @@ var carTypes = [vSedan,vCoupe,vSUV,vCaravan,vTruck,vTruckLarge];
 var carSelection = ["sedan","coupe","suv","caravan","truck","truckL"];
 //-----------basic-vip-showroom-machine
 var pSedan = ["80","180","600","180"];
-var pCoupe = ["80","140","400",pSedan[3]];
+var pCoupe = ["80","140","400","400"];
 var pSUV = ["140","200","800","250"];
 var pCaravan = ["220","280","1000","400"];
 var pTruck = ["130","200","800","280"];
